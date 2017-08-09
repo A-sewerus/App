165078d2c90b44e9661a21032aae9d824e5c2d3c
Andrey <pinci_super_star@inbox.ru>
2017-08-09 12:53:35 +0300

    post запрос

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const db = require('./db')
 const utils = require('./utils')
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 
 function initRoutes() {
-    const router = express.Router();
+        const router = express.Router();
+        router.use(bodyParser.json());
+        router.use(bodyParser.urlencoded({ extended: true }));
 
-    router.get('/task', function (req, res) {
-        res.send(utils.randomPhrase(db.getRandomPhrase()));
-    });
-    return router;
+        // router.get('/task', function (req, res) {
+        //         res.send(utils.randomPhrase(db.getRandomPhrase()));
+        // });
+        // return router;
+
+        router.post('/answer', upload.array(), function (req, res) {
+                var userAnswer = { id: req.body[0].id, answer: req.body[0].answer }
+                //console.log(userAnswer);
+                res.send(utils.checkAnswer(userAnswer));
+        });
+        return router;
 }
 
+
 module.exports = {
-    initRoutes: initRoutes
+        initRoutes: initRoutes
 }
